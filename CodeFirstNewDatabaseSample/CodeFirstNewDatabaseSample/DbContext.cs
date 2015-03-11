36ffd349b671f70cda149f8547263d92a7e4b91a
qianhaopower <qianhaopower@gmail.com>
2015-03-11 17:37:44 +1100

    initial push

---
+++ CodeFirstNewDatabaseSample/CodeFirstNewDatabaseSample/DbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace CodeFirstNewDatabaseSample
+{
+    public class BloggingContext : DbContext
+    {
+
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<User>().Property(u => u.DisplayName).HasColumnName("display_name"); 
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
