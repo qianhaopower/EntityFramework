b68588b6205a4c80503dfa1e5963364775c4ed21
qianhaopower <qianhaopower@gmail.com>
2015-03-16 15:28:54 +1100

    AsyncAndMoq

---
+++ AsyncDemo/AsyncDemo/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.Data.Entity; 
+using System.Threading.Tasks;
+using System.Threading; 
+
+namespace AsyncDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var task =  PerformDatabaseOperations();
+
+            Console.WriteLine();
+            Console.WriteLine("Quote of the day");
+            Console.WriteLine(" Don't worry about the world coming to an end today... ");
+            Console.WriteLine(" It's already tomorrow in Australia.");
+
+
+           // task.Wait(); 
+
+            for (int i = 0 ; i<= 50 ;i++)
+            {
+                Thread.Sleep(10);
+                   Console.WriteLine("Quote" + i);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        public static async Task PerformDatabaseOperations()
+        {
+            using (var db = new BloggingContext())
+            {
+                // log SQL to the console
+                db.Database.Log = Console.Write; 
+                // Create a new blog and save it 
+                db.Blogs.Add(new Blog
+                {
+                    Name = "Test Blog #" + (db.Blogs.Count() + 1)
+                });
+                await db.SaveChangesAsync();
+                Console.WriteLine("Between  two awaits");
+
+                // Query for all blogs ordered by name 
+                var blogs = await (from b in db.Blogs
+                             orderby b.Name
+                             select b).ToListAsync();
+
+                // Write all blogs out to Console 
+                Console.WriteLine();
+                Console.WriteLine("All blogs:");
+                foreach (var blog in blogs)
+                {
+                    Thread.Sleep(10);
+                    Console.WriteLine(" " + blog.Name);
+                }
+            }
+        }
+    }
+}
---
+++ AsyncDemo/MoqTestingDemo/Model.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity; 
+
+namespace MoqTestingDemo
+{
+   
+        public class BloggingContext : DbContext
+        {
+            public virtual DbSet<Blog> Blogs { get; set; }
+            public virtual DbSet<Post> Posts { get; set; }
+        }
+
+        public class Blog
+        {
+            public int BlogId { get; set; }
+            public string Name { get; set; }
+            public string Url { get; set; }
+
+            public virtual List<Post> Posts { get; set; }
+        }
+
+        public class Post
+        {
+            public int PostId { get; set; }
+            public string Title { get; set; }
+            public string Content { get; set; }
+
+            public int BlogId { get; set; }
+            public virtual Blog Blog { get; set; }
+        } 
+    
+}
